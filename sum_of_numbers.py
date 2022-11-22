ceb3b75634ec3cd354dc0660eb6859e6f0ff9532
TsvetkoPetkov <115669197+TsvetkoPetkov@users.noreply.github.com>
2022-11-22 22:27:30 +0200

    Update sum_of_numbers.py

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -4,13 +4,14 @@ a = number
 
 convert_to_string = str(a)
 total_sum = 0
+output = ''
 
 for i in range(1, number+1):
     b = convert_to_string * i
-    print(b)
     convert_to_int = int(b)
     total_sum += convert_to_int
+    output += b + " + "
 
-print(total_sum)
-
-
+output_two = output[:-2]
+print(output_two, end='')
+print(f"= {total_sum}")
